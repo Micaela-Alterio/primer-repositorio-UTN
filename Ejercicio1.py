384fd4f4854a138b41e20bf199a8af4eee2f6b6a
Micaela-Alterio <mikalterio@gmail.com>
2025-03-28 22:15:33 -0300

    Se agregaron comentarios

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,6 @@
+#Se define variable nombre
 nombre="Mica"
+#Se define variable apellido
 apellido="Alterio"
+#Se imprime mensaje por consola
 print(f"Hola, soy {nombre} {apellido} alumna de programación")
