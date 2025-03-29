5f169ba87a7d2416a6df60e15037cc64f6dd7b05
Micaela-Alterio <mikalterio@gmail.com>
2025-03-28 22:09:16 -0300

    cambio en la rama main

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,4 @@
-nombre="Mica"
-apellido="Alterio"
+nombre=input("Ingrese su nombre")
+apellido=input("Ingrese su apellido")
 print(f"Hola, soy {nombre} {apellido} alumna de programación")
+print ("Hola")
